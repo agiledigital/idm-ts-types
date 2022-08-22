58e7c1f220d21a100f945135cbcac8b824e98ca1
Joel Pearson <joel.pearson@gmail.com>
2022-08-22 22:30:55 +1000

    Add create and patch documentation

--- lib/idm-ts.ts
+++ lib/idm-ts.ts
@@ -119,16 +119,73 @@ export class IDMObject<T extends IDMObjectType<string>, D extends IDMObjectType<
     return openidm.read(`${this.type}/${id}`, params, unCheckedFields ? unCheckedFields : fields);
   }
 
+  /**
+   * This function creates a new resource object returning the newly created object with only the specified fields and the type narrowed accordingly.
+   * 
+   * @example
+   * ```ts
+   * idm.managed.user.create(
+   *  "<managedUserId>",
+   *  { userName: "abc123", givenName: "Babs", sn: "Jansen", mail: "babs@babs.com"},
+   *  { fields: ["userName", "givenName"] }
+   * )
+   * ```
+   * 
+   * @param newResourceId - The identifier of the object to be created, if the client is supplying the ID. If the server should generate the ID, pass null here.
+   * @param content - The content of the object to be created.
+   * @param options - Options object which must contain an array of checked field
+   * @returns The created resource object with it's type narrowed to the specified fields.
+   */
   public create<F extends Fields<T>>(
     newResourceId: string | null,
     content: WithOptionalId<T>,
     options: { readonly params?: object; readonly fields: F[] }
   ): ResultType<T, F>;
+
+  /**
+   * This function creates a new resource object returning the newly created object with only the specified unchecked fields.
+   * 
+   * This unchecked version is essentially an escape hatch to the checked version above. The following circumstances must use an escape hatch:
+   * 1. Wildcards such as `*_ref`, `*` or `manager/*`
+   * 2. Relationship fields such as `manager/givenName` or "reports/*&#47;givenName"
+   * 
+   * @example
+   * ```ts
+   * idm.managed.user.create(
+   *  "<managedUserId>",
+   *  { userName: "abc123", givenName: "Babs", sn: "Jansen", mail: "babs@babs.com"},
+   *  { uncheckedFields: ["*"] }
+   * )
+   * ```
+   * 
+   * @param newResourceId - The identifier of the object to be created, if the client is supplying the ID. If the server should generate the ID, pass null here.
+   * @param content - The content of the object to be created.
+   * @param options - Options object which must contain an array of unchecked fields.
+   * @returns The created resource object with it's type allowing all fields as TypeScript won't know which fields you have chosen.
+   */
   public create<F extends Fields<T>>(
     newResourceId: string | null,
     content: WithOptionalId<T>,
     options: { readonly params?: object; readonly unCheckedFields: string[] }
   ): T & Revision;
+
+
+  /**
+   * This function creates a new resource object returning the newly created object with the default fields.
+   * 
+   * @example
+   * ```ts
+   * idm.managed.user.create(
+   *  "<managedUserId>",
+   *  { userName: "abc123", givenName: "Babs", sn: "Jansen", mail: "babs@babs.com"}
+   * )
+   * ```
+   * 
+   * @param newResourceId - The identifier of the object to be created, if the client is supplying the ID. If the server should generate the ID, pass null here.
+   * @param content - The content of the object to be created.
+   * @param options - Options object which can contain params, but no fields.
+   * @returns The created resource object with it's type narrowed to the default fields.
+   */
   public create<F extends Fields<T>>(newResourceId: string | null, content: WithOptionalId<T>, options?: { readonly params?: object }): D & Revision;
   public create<F extends Fields<T>>(
     newResourceId: string | null,
@@ -138,18 +195,331 @@ export class IDMObject<T extends IDMObjectType<string>, D extends IDMObjectType<
     return openidm.create(this.type, newResourceId, content, params, unCheckedFields ? unCheckedFields : fields);
   }
 
+  /**
+   * This function performs a partial modification of a managed or system object. Unlike the update function, only the modified attributes are provided, not the entire object. It returns the modified object with only the specified fields and the type narrowed accordingly.
+   * 
+   * @example
+   * A `remove` operation removes a property if the value of that property equals the specified value, or if no value is specified in the request. The following example `value` removes the `marital_status` property from the object, if the value of that property is `single`:
+   * 
+   * ```json
+   * [
+   *     {
+   *         "operation": "remove",
+   *         "field": "marital_status",
+   *         "value": "single"
+   *     }
+   * ]
+   * ```
+   * For fields whose value is an array, it’s not necessary to know the position of the value in the array, as long as you specify the full object. If the full object is found in the array, that value is removed. The following example removes user adonnelly from bjensen’s `reports`:
+   * 
+   * ```json
+   * {
+   *     "operation": "remove",
+   *     "field": "/manager",
+   *     "value": {
+   *       "_ref": "managed/user/adonnelly",
+   *       "_refResourceCollection": "managed/user",
+   *       "_refResourceId": "adonnelly",
+   *       "_refProperties": {
+   *         "_id": "ed6620e4-98ba-410c-abc0-e06dc1be7aa7",
+   *         "_rev": "000000008815942b"
+   *       }
+   *     }
+   * }
+   * ```
+   * If an invalid value is specified (that is a value that does not exist for that property in the current object) the patch request is silently ignored.
+   * 
+   * A replace operation replaces an existing value, or adds a value if no value exists.
+   * 
+   * @example
+   * Patching an object to add a value to an array as an unchecked patch because checked patches require the field name to match the resource attribute name exactly without slashes or other additional characters:
+   * ```ts
+   * idm.managed.role.patch(
+   *  role._id,
+   *  null,
+   *  {
+   *    uncheckedPatches: [{"operation":"add", "field":"/members/-", "value": {"_ref":"managed/user/" + user._id}}]
+   *  },
+   *  { fields: ["userName", "givenName"] }
+   * );
+   * ```
+   * 
+   * @example
+   * Patching an object with both a checked and unchecked patch:
+   * ```ts
+   * idm.managed.role.patch(
+   *  role._id,
+   *  null,
+   *  {
+   *    checkedPatches: [{"operation":"replace", "field":"givenName", "value": "Babs"}]
+   *    uncheckedPatches: [{"operation":"add", "field":"/members/-", "value": {"_ref":"managed/user/" + user._id}}]
+   *  },
+   *  { fields: ["userName", "givenName"] }
+   * );
+   * ```
+   * 
+   * @example
+   * Patching an object to remove an existing property:
+     * 
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"remove", "field":"marital_status", "value":"single"}],
+   *  { fields: ["userName", "givenName"] }
+   * );
+   * ```
+     * 
+   * @example
+   * Patching an object to replace a field value:
+     * 
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"replace", "field":"password", "value":"Passw0rd"}],
+   *  { fields: ["userName", "givenName"] }
+   * );
+   * ```
+     * 
+   * @example
+   * Patching an object to increment an integer value:
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"increment","field":"/age","value":1}],
+   *  { fields: ["userName", "givenName"] }
+   * );
+   * ```
+   * 
+   * @param id - The identifier of the object to be patched
+   * @param rev - The revision of the object to be updated. Use null if the object is not subject to revision control, or if you want to skip the revision check and update the object, regardless of the revision.
+   * @param value - An array of one or more JSON patches with checked fields or an object that contains an unchecked patches with optional checked patches.
+   * @param options Options object which must contain an array of checked fields.
+   * @returns The modified resource object with it's type narrowed to the specified fields.
+   */
   public patch<F extends Fields<T>>(
     id: string,
     rev: string | null,
     value: CompositePatchOpts<T>,
     options: { readonly params?: object; readonly fields: F[] }
   ): ResultType<T, F>;
+
+  /**
+   * This function performs a partial modification of a managed or system object. Unlike the update function, only the modified attributes are provided, not the entire object. It returns the modified object with only the specified unchecked fields.
+   * 
+   * This unchecked field values version is essentially an escape hatch to the checked version above. The following circumstances must use an escape hatch:
+   * 1. Wildcards such as `*_ref`, `*` or `manager/*`
+   * 2. Relationship fields such as `manager/givenName` or "reports/*&#47;givenName"
+   * 
+   * @example
+   * A `remove` operation removes a property if the value of that property equals the specified value, or if no value is specified in the request. The following example `value` removes the `marital_status` property from the object, if the value of that property is `single`:
+   * 
+   * ```json
+   * [
+   *     {
+   *         "operation": "remove",
+   *         "field": "marital_status",
+   *         "value": "single"
+   *     }
+   * ]
+   * ```
+   * For fields whose value is an array, it’s not necessary to know the position of the value in the array, as long as you specify the full object. If the full object is found in the array, that value is removed. The following example removes user adonnelly from bjensen’s `reports`:
+   * 
+   * ```json
+   * {
+   *     "operation": "remove",
+   *     "field": "/manager",
+   *     "value": {
+   *       "_ref": "managed/user/adonnelly",
+   *       "_refResourceCollection": "managed/user",
+   *       "_refResourceId": "adonnelly",
+   *       "_refProperties": {
+   *         "_id": "ed6620e4-98ba-410c-abc0-e06dc1be7aa7",
+   *         "_rev": "000000008815942b"
+   *       }
+   *     }
+   * }
+   * ```
+   * If an invalid value is specified (that is a value that does not exist for that property in the current object) the patch request is silently ignored.
+   * 
+   * A replace operation replaces an existing value, or adds a value if no value exists.
+   * 
+   * @example
+   * Patching an object to add a value to an array as an unchecked patch because checked patches require the field name to match the resource attribute name exactly without slashes or other additional characters:
+   * ```ts
+   * idm.managed.role.patch(
+   *  role._id,
+   *  null,
+   *  {
+   *    uncheckedPatches: [{"operation":"add", "field":"/members/-", "value": {"_ref":"managed/user/" + user._id}}]
+   *  },
+   *  { uncheckedFields: ["*"] }
+   * );
+   * ```
+   * 
+   * @example
+   * Patching an object with both a checked and unchecked patch:
+   * ```ts
+   * idm.managed.role.patch(
+   *  role._id,
+   *  null,
+   *  {
+   *    checkedPatches: [{"operation":"replace", "field":"givenName", "value": "Babs"}]
+   *    uncheckedPatches: [{"operation":"add", "field":"/members/-", "value": {"_ref":"managed/user/" + user._id}}]
+   *  },
+   *  { uncheckedFields: ["*"] }
+   * );
+   * ```
+   * 
+   * @example
+   * Patching an object to remove an existing property:
+     * 
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"remove", "field":"marital_status", "value":"single"}],
+   *  { uncheckedFields: ["*"] }
+   * );
+   * ```
+     * 
+   * @example
+   * Patching an object to replace a field value:
+     * 
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"replace", "field":"password", "value":"Passw0rd"}],
+   *  { uncheckedFields: ["*"] }
+   * );
+   * ```
+     * 
+   * @example
+   * Patching an object to increment an integer value:
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"increment","field":"/age","value":1}],
+   *  { uncheckedFields: ["*"] }
+   * );
+   * ```
+   * 
+   * @param id - The identifier of the object to be patched
+   * @param rev - The revision of the object to be updated. Use null if the object is not subject to revision control, or if you want to skip the revision check and update the object, regardless of the revision.
+   * @param value - An array of one or more JSON patches with checked fields or an object that contains an unchecked patches with optional checked patches.
+   * @param options Options object which must contain an array of checked fields.
+   * @returns The modified resource object with it's type allowing all fields as TypeScript won't know which fields you have chosen.
+   */
   public patch<F extends Fields<T>>(
     id: string,
     rev: string | null,
     value: CompositePatchOpts<T>,
     options: { readonly params?: object; readonly unCheckedFields: F[] }
   ): T & Revision;
+
+  /**
+   * This function performs a partial modification of a managed or system object. Unlike the update function, only the modified attributes are provided, not the entire object. It returns the modified object with the default fields.
+   * 
+   * @example
+   * A `remove` operation removes a property if the value of that property equals the specified value, or if no value is specified in the request. The following example `value` removes the `marital_status` property from the object, if the value of that property is `single`:
+   * 
+   * ```json
+   * [
+   *     {
+   *         "operation": "remove",
+   *         "field": "marital_status",
+   *         "value": "single"
+   *     }
+   * ]
+   * ```
+   * For fields whose value is an array, it’s not necessary to know the position of the value in the array, as long as you specify the full object. If the full object is found in the array, that value is removed. The following example removes user adonnelly from bjensen’s `reports`:
+   * 
+   * ```json
+   * {
+   *     "operation": "remove",
+   *     "field": "/manager",
+   *     "value": {
+   *       "_ref": "managed/user/adonnelly",
+   *       "_refResourceCollection": "managed/user",
+   *       "_refResourceId": "adonnelly",
+   *       "_refProperties": {
+   *         "_id": "ed6620e4-98ba-410c-abc0-e06dc1be7aa7",
+   *         "_rev": "000000008815942b"
+   *       }
+   *     }
+   * }
+   * ```
+   * If an invalid value is specified (that is a value that does not exist for that property in the current object) the patch request is silently ignored.
+   * 
+   * A replace operation replaces an existing value, or adds a value if no value exists.
+   * 
+   * @example
+   * Patching an object to add a value to an array as an unchecked patch because checked patches require the field name to match the resource attribute name exactly without slashes or other additional characters:
+   * ```ts
+   * idm.managed.role.patch(
+   *  role._id,
+   *  null,
+   *  {
+   *    uncheckedPatches: [{"operation":"add", "field":"/members/-", "value": {"_ref":"managed/user/" + user._id}}]
+   *  }
+   * );
+   * ```
+   * 
+   * @example
+   * Patching an object with both a checked and unchecked patch:
+   * ```ts
+   * idm.managed.role.patch(
+   *  role._id,
+   *  null,
+   *  {
+   *    checkedPatches: [{"operation":"replace", "field":"givenName", "value": "Babs"}]
+   *    uncheckedPatches: [{"operation":"add", "field":"/members/-", "value": {"_ref":"managed/user/" + user._id}}]
+   *  }
+   * );
+   * ```
+   * 
+   * @example
+   * Patching an object to remove an existing property:
+     * 
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"remove", "field":"marital_status", "value":"single"}]
+   * );
+   * ```
+     * 
+   * @example
+   * Patching an object to replace a field value:
+     * 
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"replace", "field":"password", "value":"Passw0rd"}]
+   * );
+   * ```
+     * 
+   * @example
+   * Patching an object to increment an integer value:
+   * ```ts
+   * idm.managed.user.patch(
+   *  user._id,
+   *  null,
+   *  [{"operation":"increment","field":"/age","value":1}]
+   * );
+   * ```
+   * 
+   * @param id - The identifier of the object to be patched
+   * @param rev - The revision of the object to be updated. Use null if the object is not subject to revision control, or if you want to skip the revision check and update the object, regardless of the revision.
+   * @param value - An array of one or more JSON patches with checked fields or an object that contains an unchecked patches with optional checked patches.
+   * @param options Options object which can contain params, but no fields.
+   * @returns The modified resource object with it's type narrowed to the default fields.
+   */
   public patch<F extends Fields<T>>(id: string, rev: string | null, value: CompositePatchOpts<T>, options?: { readonly params?: object }): D & Revision;
   public patch<F extends Fields<T>>(
     id: string,
